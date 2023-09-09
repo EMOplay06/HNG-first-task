275d0f9dda9726af92bf198d45438251505e5b57
EMOplay06 <michaelechela@gmail.com>
2023-09-09 14:39:03 +0100

    code adjusted

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-// Get references to the elements
 const currentDayElement = document.getElementById("currentDay");
 const currentUTCElement = document.getElementById("currentUTC");
 
-// Function to update the current day
+
 function updateCurrentDay() {
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const currentDate = new Date();
@@ -10,13 +9,13 @@ function updateCurrentDay() {
     currentDayElement.textContent = currentDayOfWeek;
 }
 
- //Function to update the current UTC time
+
  function updateCurrentUTC() {
      const currentUTCTime = new Date();
      const currentTimeString = currentUTCTime.toISOString();
      currentUTCElement.textContent = currentUTCTime;
  }
 
-// Call the functions to update the current day and UTC time
+
 updateCurrentDay();
 updateCurrentUTC();
