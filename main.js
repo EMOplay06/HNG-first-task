95208d989f0111028ba015cdc89d1d0a50d1f1f6
EMOplay06 <michaelechela@gmail.com>
2023-09-10 16:48:26 +0100

    task completed

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-const currentDayElement = document.getElementById("currentDay");
-const currentUTCElement = document.getElementById("currentUTC");
+// const currentDayElement = document.getElementById("currentDay");
+// const currentUTCElement = document.getElementById("currentUTC");
 
 
-function updateCurrentDay() {
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const currentDate = new Date();
-    const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
-    currentDayElement.textContent = currentDayOfWeek;
-}
+// function updateCurrentDay() {
+//     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//     const currentDate = new Date();
+//     const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+//     currentDayElement.textContent = currentDayOfWeek;
+// }
 
 
- function updateCurrentUTC() {
-     const currentUTCTime = new Date();
-     const currentTimeString = currentUTCTime.toISOString();
-     currentUTCElement.textContent = currentUTCTime;
+//  function updateCurrentUTC() {
+//      const currentUTCTime = new Date();
+//      const currentTimeString = currentUTCTime.toISOString();
+//      currentUTCElement.textContent = currentUTCTime;
      
- }
+//  }
 
 
-updateCurrentDay();
-updateCurrentUTC();
+// updateCurrentDay();
+// updateCurrentUTC();
+document.getElementById('currentUTC').innerHTML = Date.now()
